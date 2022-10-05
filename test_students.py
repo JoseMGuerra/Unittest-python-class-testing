851d44c31e0979ac7bc691bb064ff09a53105a2c
JoseMGuerra <Josem_guerra@hotmail.com>
2022-10-05 10:03:07 +0000

    Unittest python class course

---
+++ test_students.py
@@ -0,0 +1,75 @@
+import unittest
+from student import Student
+from datetime import timedelta
+from unittest.mock import patch
+
+
+class TestStudent(unittest.TestCase):
+    '''Todo - ie. implementation:
+    - Request student start date,
+    - Factor in leap year when calculating student end date
+    - Display whether a student's given an extension
+    - Keep track of students scores
+
+    '''
+
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+
+    def setUp(self):
+        print("setUp")
+        self.student = Student("John", "Doe")
+
+    def tearDown(self):
+        print("tearDown")
+
+    def test_full_name(self):
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
+
+    def test_student_email(self):
+        print("test_student_email")
+        self.assertEqual(self.student.student_email, "john.doe@email.com")
+
+    def test_student_start_date(self):
+        print("test_student_start_date")
+        pass
+
+    def test_apply_extension(self):
+        print("test_apply_extension")
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+        self.assertEqual(self.
+        student.end_date, old_end_date + timedelta(days=5))
+
+    def test_alert_santa(self):
+        print("test_alert_santa")
+        self.student.alert_santa()
+
+        self.assertTrue(self.student.naughty_list)
+
+    def test_course_schedule_success(self):
+        print("test_course_schedule_success")
+        with patch("student.requests.get") as mock_get:
+            mock_get.return_value.ok = True
+            mock_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+    def test_course_schedule_failed(self):
+        print("test_course_schedule_failed")
+        with patch("student.requests.get") as mock_get:
+            mock_get.return_value.ok = False
+            mock_get.return_value.text = "Something went wrong with the request!"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request!")
+
+if __name__ == "__main__":
+    unittest.main()
